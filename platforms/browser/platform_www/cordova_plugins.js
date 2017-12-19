fea4c592ae0b149d23cf8a35263bee964eaf3015
Sushant Khurana <sushant.khurana.sukh@gmail.com>
2017-12-19 17:25:28 +0530

    qw

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,170 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassError.js",
+        "id": "cordova-plugin-device-orientation.CompassError",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "CompassError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassHeading.js",
+        "id": "cordova-plugin-device-orientation.CompassHeading",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "CompassHeading"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/compass.js",
+        "id": "cordova-plugin-device-orientation.compass",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "navigator.compass"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/src/browser/CompassProxy.js",
+        "id": "cordova-plugin-device-orientation.CompassProxy",
+        "pluginId": "cordova-plugin-device-orientation",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassError.js",
+        "id": "cordova-plugin-device-orientation.CompassError",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "CompassError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassHeading.js",
+        "id": "cordova-plugin-device-orientation.CompassHeading",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "CompassHeading"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/compass.js",
+        "id": "cordova-plugin-device-orientation.compass",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "navigator.compass"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/src/browser/CompassProxy.js",
+        "id": "cordova-plugin-device-orientation.CompassProxy",
+        "pluginId": "cordova-plugin-device-orientation",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/src/browser/network.js",
+        "id": "cordova-plugin-network-information.NetworkInfoProxy",
+        "pluginId": "cordova-plugin-network-information",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/src/browser/network.js",
+        "id": "cordova-plugin-network-information.NetworkInfoProxy",
+        "pluginId": "cordova-plugin-network-information",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/src/browser/SplashScreenProxy.js",
+        "id": "cordova-plugin-splashscreen.SplashScreenProxy",
+        "pluginId": "cordova-plugin-splashscreen",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/src/browser/SplashScreenProxy.js",
+        "id": "cordova-plugin-splashscreen.SplashScreenProxy",
+        "pluginId": "cordova-plugin-splashscreen",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-phone-calllog-watch/www/CallLog.js",
+        "id": "cordova-phone-calllog-watch.CallLog",
+        "pluginId": "cordova-phone-calllog-watch",
+        "clobbers": [
+            "window.callLogWatch"
+        ]
+    },
+    {
+        "file": "plugins/cordova-phone-calllog-watch/test/tests.js",
+        "id": "cordova-phone-calllog-watch.tests",
+        "pluginId": "cordova-phone-calllog-watch"
+    },
+    {
+        "file": "plugins/callsplugin/./www/CallsPlugin.js",
+        "id": "callsplugin.CallsPlugin",
+        "pluginId": "callsplugin",
+        "clobbers": [
+            "calls"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device-orientation": "1.0.7",
+    "cordova-plugin-network-information": "1.3.4",
+    "cordova-plugin-splashscreen": "4.1.0",
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-phone-calllog-watch": "2.4",
+    "callsplugin": "0.2"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/browser/platform_www/plugins/cordova-phone-calllog-watch/www/CallLog.js
@@ -0,0 +1,35 @@
+cordova.define("cordova-phone-calllog-watch.CallLog", function(require, exports, module) { function CallLog() {
+}
+
+CallLog.prototype.list = function (period, successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "CallLog", "list", [period]);
+};
+
+CallLog.prototype.contact = function (phoneNumber, successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "CallLog", "contact", [phoneNumber]);
+};
+
+CallLog.prototype.show = function (phoneNumber, successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "CallLog", "show", [phoneNumber]);
+};
+
+CallLog.prototype.delete = function (id, successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "CallLog", "delete", [id]);
+};
+
+CallLog.prototype.insert = function (args, successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "CallLog", "insert", args);
+};
+
+CallLog.install = function () {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+
+  window.callLogWatch = new CallLog();
+  return window.callLogWatch;
+};
+
+cordova.addConstructor(CallLog.install);
+
+});
